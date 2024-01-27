62e62c5560cc91ef1d2102251522a30a5fc289bc
neph <neph3779@gmail.com>
2024-01-27 20:55:04 +0900

    프로젝트 파일 설정 및 환경세팅

---
+++ src/main/java/com/neph/WuriSool/WuriSoolApplication.java
@@ -0,0 +1,13 @@
+package com.neph.WuriSool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuriSoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WuriSoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neph/WuriSool/WuriSoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neph.WuriSool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuriSoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
